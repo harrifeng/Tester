5324943975d74346f17f8b49663ec3819879a8f9
Harri Feng <harrifeng@gmail.com>
2016-06-25 11:22:45 +0800

    utf-8 and ascii

--- world.py
+++ world.py
@@ -6,3 +6,6 @@ print template.render(name='Haoran Feng')
 m = Template(u"{% set a, b = 'foo', 'föö' %}").module
 print type(m.a), m.a
 print type(m.b), m.b.encode('utf-8')
+# UTF-8对于单字节的符号，字节的第一位设为0，后面7位为这个符号的unicode码。
+# 因此对于英语字母，UTF-8编码和ASCII码是相同的。
+print type(m.a), m.a.encode('utf-8')

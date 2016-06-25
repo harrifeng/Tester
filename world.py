37ff54ff03378f30d137c8dc0fed1aeb064d210a
Harri Feng <harrifeng@gmail.com>
2016-06-25 11:06:26 +0800

    recommend way to do this

--- world.py
+++ world.py
@@ -1,11 +1,8 @@
 # coding: utf-8
-import sys
 from jinja2 import Template
 template = Template('Hello {{ name }} !')
 print template.render(name='Haoran Feng')
 
 m = Template(u"{% set a, b = 'foo', 'föö' %}").module
-print m.a
-reload(sys)
-sys.setdefaultencoding('utf-8')
-print m.b
+print type(m.a), m.a
+print type(m.b), m.b.encode('utf-8')

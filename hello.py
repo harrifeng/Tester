4a8e84238de209fdd77b984a414c1685d7470870
Harri Feng <harrifeng@gmail.com>
2016-06-25 10:20:47 +0800

    new understand

--- hello.py
+++ hello.py
@@ -4,5 +4,8 @@ from Tester import Mamalls
 env = Environment(loader=PackageLoader("Tester", "templates"))
 print env
 
+template = env.get_template('mytemplate.html')
+print template.render(the='variable', go='here')
+
 m = Mamalls()
 m.printMember()
